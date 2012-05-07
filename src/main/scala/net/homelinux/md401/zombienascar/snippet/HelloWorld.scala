2ea3681b6b980944bcee45258fd17873610529f4
Michael Donaghy <md401@srcf.ucam.org>
2012-05-07 13:07:51 +0100

    Initial maven skeleton project

---
+++ src/main/scala/net/homelinux/md401/zombienascar/snippet/HelloWorld.scala
@@ -0,0 +1,25 @@
+package net.homelinux.md401.zombienascar {
+package snippet {
+
+import _root_.scala.xml.{NodeSeq, Text}
+import _root_.net.liftweb.util._
+import _root_.net.liftweb.common._
+import _root_.java.util.Date
+import net.homelinux.md401.zombienascar.lib._
+import Helpers._
+
+class HelloWorld {
+  lazy val date: Box[Date] = DependencyFactory.inject[Date] // inject the date
+
+  def howdy(in: NodeSeq): NodeSeq =
+  Helpers.bind("b", in, "time" -> date.map(d => Text(d.toString)))
+
+  /*
+   lazy val date: Date = DependencyFactory.time.vend // create the date via factory
+
+   def howdy(in: NodeSeq): NodeSeq = Helpers.bind("b", in, "time" -> date.toString)
+   */
+}
+
+}
+}
