facf2af8ed7fa9dea3248deb80a3a6f242d94b3f
Michael Donaghy <md401@srcf.ucam.org>
2012-05-07 16:06:15 +0100

    Maybe this is what it doesn't like?

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -18,17 +18,17 @@ import _root_.net.homelinux.md401.zombienascar.model._
  */
 class Boot {
   def boot {
-    if (!DB.jndiJdbcConnAvailable_?) {
-      val vendor = 
-	new StandardDBVendor(Props.get("db.driver") openOr "org.h2.Driver",
-			     Props.get("db.url") openOr 
-			     "jdbc:h2:lift_proto.db;AUTO_SERVER=TRUE",
-			     Props.get("db.user"), Props.get("db.password"))
-
-      LiftRules.unloadHooks.append(vendor.closeAllConnections_! _)
-
-      DB.defineConnectionManager(DefaultConnectionIdentifier, vendor)
-    }
+//    if (!DB.jndiJdbcConnAvailable_?) {
+//      val vendor = 
+//	new StandardDBVendor(Props.get("db.driver") openOr "org.h2.Driver",
+//			     Props.get("db.url") openOr 
+//			     "jdbc:h2:lift_proto.db;AUTO_SERVER=TRUE",
+//			     Props.get("db.user"), Props.get("db.password"))
+//
+//      LiftRules.unloadHooks.append(vendor.closeAllConnections_! _)
+//
+//      DB.defineConnectionManager(DefaultConnectionIdentifier, vendor)
+//    }
 
     // where to search snippet
     LiftRules.addToPackages("net.homelinux.md401.zombienascar")
