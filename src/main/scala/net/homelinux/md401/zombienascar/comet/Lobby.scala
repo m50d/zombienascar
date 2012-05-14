54ce6a1f130b6560e3afb13012f48f162294092e
Michael Donaghy <md401@srcf.ucam.org>
2012-05-14 20:50:25 +0100

    Put a chat thing in there

---
+++ src/main/scala/net/homelinux/md401/zombienascar/comet/Lobby.scala
@@ -0,0 +1,65 @@
+package net.homelinux.md401.zombienascar.comet
+import net.liftweb.actor.LiftActor
+import net.liftweb.http.ListenerManager
+import net.liftweb.http.CometActor
+import net.liftweb.http.CometListener
+import net.liftweb.util.ClearClearable
+
+/**
+ * A singleton that provides chat features to all clients.
+ * It's an Actor so it's thread-safe because only one
+ * message will be processed at once.
+ */
+object ChatServer extends LiftActor with ListenerManager {
+  private var msgs = Vector("Welcome") // private state
+
+  /**
+   * When we update the listeners, what message do we send?
+   * We send the msgs, which is an immutable data structure,
+   * so it can be shared with lots of threads without any
+   * danger or locking.
+   */
+  def createUpdate = msgs
+
+  /**
+   * process messages that are sent to the Actor.  In
+   * this case, we're looking for Strings that are sent
+   * to the ChatServer.  We append them to our Vector of
+   * messages, and then update all the listeners.
+   */
+  override def lowPriority = {
+    case s: String => msgs :+= s; updateListeners()
+  }
+}
+
+/**
+ * The screen real estate on the browser will be represented
+ * by this component.  When the component changes on the server
+ * the changes are automatically reflected in the browser.
+ */
+class Lobby extends CometActor with CometListener {
+  private var msgs: Vector[String] = Vector() // private state
+
+  /**
+   * When the component is instantiated, register as
+   * a listener with the ChatServer
+   */
+  def registerWith = ChatServer
+
+  /**
+   * The CometActor is an Actor, so it processes messages.
+   * In this case, we're listening for Vector[String],
+   * and when we get one, update our private state
+   * and reRender() the component.  reRender() will
+   * cause changes to be sent to the browser.
+   */
+  override def lowPriority = {
+    case v: Vector[String] => msgs = v; reRender()
+  }
+
+  /**
+   * Put the messages in the li elements and clear
+   * any elements that have the clearable class.
+   */
+  def render = "li *" #> msgs & ClearClearable
+}
