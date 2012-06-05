d85f17121c801059feb7c0f2c408d56e31ea59b7
Michael Donaghy <md401@srcf.ucam.org>
2012-06-05 18:08:53 +0100

    Some stuff I'm experimenting with, trying to understand

--- src/main/scala/net/homelinux/md401/zombienascar/snippet/Hand.scala
+++ src/main/scala/net/homelinux/md401/zombienascar/snippet/Hand.scala
@@ -4,6 +4,9 @@ import net.homelinux.md401.zombienascar.backend.Deck
 import net.liftweb.http.js.JE.JsRaw
 import java.util.UUID
 import net.liftweb.http.SHtml
+import net.liftweb.http.JsonHandler
+import net.liftweb.http.js.JsCmd
+import net.liftweb.util.JsonCmd
 
 class hand {
   def render(): NodeSeq = {
@@ -12,4 +15,16 @@ class hand {
       val id = UUID.randomUUID().toString()
       <img src={c.filename} id={id}></img><script>$(function() {{$("#{id}").draggable();}});</script>})
   }
+}
+
+object json extends JsonHandler {
+  def apply(in: Any): JsCmd = { in match { 
+            case JsonCmd("processForm", _, p: Map[String, _], _) => {
+                // process the form or whatever 
+//                println("Cars = " + urlDecode(p("cars"))) 
+//                println("Name = " + urlDecode(p("name"))) 
+//                <b>{p}</b> 
+            }
+            case _ => JsCmd.unitToJsCmd()
+        }}
 }
