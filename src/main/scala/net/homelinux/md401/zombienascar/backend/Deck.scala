4e60f942c4d9103fc6740582207cbc710b4d6542
Michael Donaghy <md401@srcf.ucam.org>
2012-06-06 18:31:34 +0100

    Draw cards from the deck. note the deck can currently run out

--- src/main/scala/net/homelinux/md401/zombienascar/backend/Deck.scala
+++ src/main/scala/net/homelinux/md401/zombienascar/backend/Deck.scala
@@ -1,11 +1,15 @@
 package net.homelinux.md401.zombienascar.backend
 
-object Deck {
+object Deck { //TODO: make this an actor
 	val fullDeck: List[Card] = 
 	 List((Uturn(), 6), (Left(), 18), (Right(), 18), (Back(), 6), (Forward1(), 18), (Forward2(), 12), (Forward3(), 6)
 	     ) flatMap {
 	  case (c: Card, count: Int) => List.make(count, c)
 	}
 	var currentDeck = util.Random.shuffle(fullDeck)
-	def hand(size: Int) = currentDeck.take(size)
+	def hand(size: Int) = {
+		val (a, b) = currentDeck.splitAt(9)
+		currentDeck = b
+		a
+	}
 }
