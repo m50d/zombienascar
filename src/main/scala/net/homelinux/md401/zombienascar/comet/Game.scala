962b0b9cf4f50df098f74eb0495d6b4098d85f2f
Michael Donaghy <md401@srcf.ucam.org>
2012-06-09 00:11:26 +0100

    I think this should be displaying, but it isn't

--- src/main/scala/net/homelinux/md401/zombienascar/comet/Game.scala
+++ src/main/scala/net/homelinux/md401/zombienascar/comet/Game.scala
@@ -6,6 +6,10 @@ import net.homelinux.md401.zombienascar.backend.EuclideanSquareOrientation
 import net.homelinux.md401.zombienascar.backend.EuclideanSquarePosition
 import net.homelinux.md401.zombienascar.backend.Car
 import net.homelinux.md401.zombienascar.backend.North
+import net.liftweb.http.CometActor
+import net.liftweb.http.CometListener
+import net.liftweb.http.RenderOut
+import scala.xml.Text
 
 //Will soon become an class, but for now it's an object
 object Game extends LiftActor with ListenerManager {
@@ -19,3 +23,12 @@ object PlayerCar extends LiftActor with ListenerManager {
   var c: Car = Car(EuclideanSquarePosition(0, 0), North())
   def createUpdate = c
 } 
+
+class DisplayCar extends CometActor with CometListener {
+  var c: Car = Car(EuclideanSquarePosition(0, 0), North())
+  
+  def registerWith = PlayerCar
+  def render: RenderOut = {
+    Text("Car at " + c.position + " facing " + c.orientation)
+  }
+}
