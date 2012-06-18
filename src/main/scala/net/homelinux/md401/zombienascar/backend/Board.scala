e89549708fd0a08430c89f0fcdcf9d874ab3aac4
Michael Donaghy <md401@srcf.ucam.org>
2012-06-18 21:00:53 +0100

    Something potentially useful

--- src/main/scala/net/homelinux/md401/zombienascar/backend/Board.scala
+++ src/main/scala/net/homelinux/md401/zombienascar/backend/Board.scala
@@ -2,7 +2,19 @@ package net.homelinux.md401.zombienascar.backend
 import scala.xml.NodeSeq
 
 class Board {
-	def render: NodeSeq = {
-	  <div></div>
-	}
+  val tiles = List.tabulate(12, 12)((_, _) => BlankSquare)
+  def render: NodeSeq = {
+    <table>{
+      tiles map {
+        row =>
+          <tr>
+            {
+              row map {
+                tile: Square => <td><img src={ tile.filename }/></td>
+              }
+            }
+          </tr>
+      }
+    }</table>
+  }
 }
--- src/main/scala/net/homelinux/md401/zombienascar/backend/Squares.scala
+++ src/main/scala/net/homelinux/md401/zombienascar/backend/Squares.scala
@@ -1,7 +1,7 @@
 package net.homelinux.md401.zombienascar.backend
 
 sealed trait Square {
-  
+  def filename: String
 }
 
 object BlankSquare extends Square {
