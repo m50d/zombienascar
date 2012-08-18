b7fecda8995b546e1248549466bcd324044366df
Michael Donaghy <md401@srcf.ucam.org>
2012-08-18 09:09:33 +0100

    A bunch of backend ready to display the thing

--- src/main/scala/net/homelinux/md401/zombienascar/comet/Game.scala
+++ src/main/scala/net/homelinux/md401/zombienascar/comet/Game.scala
@@ -11,6 +11,10 @@ import net.liftweb.http.CometListener
 import net.liftweb.http.RenderOut
 import scala.xml.Text
 import net.homelinux.md401.zombienascar.backend.Card
+import net.homelinux.md401.zombienascar.backend.BlankSquare
+import net.homelinux.md401.zombienascar.backend.EuclideanSquarePosition
+import net.homelinux.md401.zombienascar.backend.EuclideanSquarePosition
+import net.homelinux.md401.zombienascar.backend.DisplayTile
 
 //Will soon become an class, but for now it's an object
 object Game extends LiftActor with ListenerManager {
@@ -32,7 +36,14 @@ object PlayerCar extends LiftActor with ListenerManager {
 } 
 
 class DisplayCar extends CometActor with CometListener {
+  val allIndexes = List.tabulate(12, 12)(EuclideanSquarePosition(_, _))
+  val allPositions = allIndexes.flatten
+  val board = allPositions map {p: EuclideanSquarePosition => (p, BlankSquare)} toMap
   var c: Car = Car(EuclideanSquarePosition(0, 0), North)
+  
+  def displayBoard = board map {
+    case (p, s) => (p, DisplayTile(s, if(c.position equals p) Some(c) else None))
+  }
   
   def registerWith = PlayerCar
   def render: RenderOut = {
