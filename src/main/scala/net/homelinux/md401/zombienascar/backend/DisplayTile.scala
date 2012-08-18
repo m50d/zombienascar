c02b828a0d7142ae6e7d6cd430b12f023ea8ba5a
Michael Donaghy <md401@srcf.ucam.org>
2012-08-18 12:13:51 +0100

    Display the car

--- src/main/scala/net/homelinux/md401/zombienascar/backend/DisplayTile.scala
+++ src/main/scala/net/homelinux/md401/zombienascar/backend/DisplayTile.scala
@@ -5,7 +5,7 @@ import scala.xml.Text
 case class DisplayTile(square: Square, car: Option[Car]) {
   def render: NodeSeq = {
     val carOrNothing : NodeSeq = car match {
-      case Some(c) => <div style="position:absolute">CAR</div>
+      case Some(c) => <div style="position:absolute"><img src="tiles/car.png"></img></div>
       case None => Text("")
     }
     carOrNothing ++ <img src={ "tiles/" + square.filename }/>
