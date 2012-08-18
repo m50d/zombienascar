e98551ce6650cb47bed5f58d01a071183c8b137c
Michael Donaghy <md401@srcf.ucam.org>
2012-08-18 08:55:47 +0100

    Display Tile class - an attempt to make something that knows if it's got a car on it

---
+++ src/main/scala/net/homelinux/md401/zombienascar/backend/DisplayTile.scala
@@ -0,0 +1,6 @@
+package net.homelinux.md401.zombienascar.backend
+import scala.xml.NodeSeq
+
+case class DisplayTile(square: Square, car: Option[Car]) {
+  def render: NodeSeq = <img src={ "tiles/" + square.filename }/> ++ car map {_ => <div style="position:absolute">CAR</div>}
+}
