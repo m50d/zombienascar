315cb00ec8ba021c3393ecb1ff3d9f19e209e181
Michael Donaghy <md401@srcf.ucam.org>
2012-08-18 09:15:49 +0100

    Not really good enough

--- src/main/scala/net/homelinux/md401/zombienascar/backend/DisplayTile.scala
+++ src/main/scala/net/homelinux/md401/zombienascar/backend/DisplayTile.scala
@@ -2,5 +2,5 @@ package net.homelinux.md401.zombienascar.backend
 import scala.xml.NodeSeq
 
 case class DisplayTile(square: Square, car: Option[Car]) {
-  def render: NodeSeq = <img src={ "tiles/" + square.filename }/> ++ car map {_ => <div style="position:absolute">CAR</div>}
+  def render: NodeSeq = <img src={ "tiles/" + square.filename }/> ++ car flatMap {_ => <div style="position:absolute">CAR</div>}
 }
